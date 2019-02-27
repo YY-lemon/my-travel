6e021fd55f0c3b068f06515168f97a93b9603270
YY-lemon <2712863042@qq.com>
2019-02-27 16:27:48 +0800

    add 搜索框功能实现

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,28 +1,96 @@
 <template>
-    <div class="search">
-      <input type="text" placeholder="输入城市名称或拼音">
+  <div class="search">
+    <div class="searchInputBox">
+      <input type="text" placeholder="输入城市名称或拼音" v-model="keyword"/>
     </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li v-for="item of list" :key="item.id" class="search-item border-bottom">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
+  </div>
 </template>
            
 <script>
-export default{
-    name: 'CitySearch'      
-}
+import Bscroll from 'better-scroll'
+export default {
+  name: "CitySearch",
+  props: {
+      cities:Object
+  },
+  data() {
+      return {
+          keyword:'',
+          list:[],
+          timer:null
+      }
+  },
+  computed: {
+      hasNoData () {
+          return !this.list.length
+      }
+  },
+  watch: {
+      keyword () {
+          if (this.timer) {
+              clearTimeout(this.timer)
+          }
+          if (!this.keyword) {
+              this.list = []
+              return
+          }
+          this.timer = setTimeout(() => {
+              const res = []
+              for(let i in this.cities) {
+                  this.cities[i].forEach((value) => {
+                      if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                          res.push(value)
+                      }
+                  });
+              }
+              this.list = res
+          },100)
+      }
+  },
+  mounted () {
+      this.scroll = new Bscroll(this.$refs.search)
+  }
+};
 </script>
 <style lang='less'scoped>
-    .search {
+.search {
+    .searchInputBox {
         height: 0.72rem;
         background-color: #00bcd4;
         padding: 0.1rem;
-        input {
-            height: 0.62rem;
-            line-height: 0.62rem;
-            width: 100%;
-            text-align: center;
-            border-radius: 0.06rem;
-            color:#666;
-            padding: 0 0.1rem;
-            box-sizing: border-box;
+            input {
+                height: 0.62rem;
+                line-height: 0.62rem;
+                width: 100%;
+                text-align: center;
+                border-radius: 0.06rem;
+                color: #666;
+                padding: 0 0.1rem;
+                box-sizing: border-box;
+            }
         }
-    }       
+    .search-content {
+        z-index: 9;
+        overflow: hidden;
+        position: absolute;
+        top: 1.68rem;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #eee;
+        .search-item {
+            line-height: 0.62rem;
+            padding-left: 0.2rem;
+            background-color: #fff;
+
+        }
+    }
+}
+
 </style>

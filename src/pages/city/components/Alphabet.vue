bbcdfaa18814c230fbc70b8ba81b311d79dccbc8
YY-lemon <2712863042@qq.com>
2019-02-27 14:09:17 +0800

    add finish 城市选择页面的所有布局

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="alphabetList">
+        <li class="item">A</li>
+    </ul>
+</template>
+           
+<script>
+export default{
+    name:'CityAlphabet'      
+}
+</script>
+<style lang='less'scoped>
+    .alphabetList {
+        position: absolute;
+        top: 1.58rem;
+        right: 0;
+        bottom: 0;
+        width: 0.4rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center; 
+        .item {
+            text-align: center;
+            line-height: 0.4rem;
+            background-color: #00bcd4;
+        }
+    }       
+</style>
